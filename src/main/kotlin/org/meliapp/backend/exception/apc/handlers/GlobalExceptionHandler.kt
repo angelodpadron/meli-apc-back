615ee2468448cd58d13be8c01457d43eef1c98eb
Angelo Padron <padron891@gmail.com>
2024-10-04 22:57:35 -0300

    improved global error handling

--- src/main/kotlin/org/meliapp/backend/exception/apc/handlers/GlobalExceptionHandler.kt
+++ src/main/kotlin/org/meliapp/backend/exception/apc/handlers/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package org.meliapp.backend.exception.apc.handlers
 import io.jsonwebtoken.JwtException
+import org.meliapp.backend.dto.ApiResponse
 import org.meliapp.backend.exception.apc.UserAlreadyRegisteredException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -11,13 +12,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException::class, JwtException::class)
-    fun handleAuthException(e: RuntimeException): ResponseEntity<String> {
-        return ResponseEntity(e.message, HttpStatus.UNAUTHORIZED)
+    fun handleAuthException(e: RuntimeException): ResponseEntity<ApiResponse<Any>> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ApiResponse(null, e.localizedMessage))
     }
 
     @ExceptionHandler(UserAlreadyRegisteredException::class)
-    fun handleUserAlreadyRegisteredException(e: UserAlreadyRegisteredException): ResponseEntity<String> {
-        return ResponseEntity(e.message, HttpStatus.BAD_REQUEST)
+    fun handleUserAlreadyRegisteredException(e: UserAlreadyRegisteredException): ResponseEntity<ApiResponse<Any>> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse(null, e.localizedMessage))
     }
 
 }
